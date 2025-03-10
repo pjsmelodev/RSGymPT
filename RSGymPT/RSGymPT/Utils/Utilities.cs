3fc3480a5f0bc3905bc7597c11e03e8d97f27548
pjsm <pjsm.lesi@gmail.com>
2025-03-10 15:54:58 +0000

    CREATE utilitarian class

---
+++ RSGymPT/RSGymPT/Utils/Utilities.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RSGymPT.Utils
+{
+    //<summary>
+    //Utility class for common functions.
+    //</summary>
+    class Utilities
+    {
+        //<summary>
+        //Pauses the terminal untill a key is pressed.
+        //</summary>
+        public static void PauseTerminal()
+        {
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey(true);
+        }
+
+
+    }
+}
