f394b3001fa31bbdabe3618f92cfada6ba747bc9
pjsm <pjsm.lesi@gmail.com>
2025-03-11 23:16:19 +0000

    UPDATED: Program

--- RSGymPT/RSGymPT/Program.cs
+++ RSGymPT/RSGymPT/Program.cs
@@ -3,8 +3,15 @@ using RSGymPT.UI;
 
 namespace RSGymPT
 {
+    //<summary>
+    //This class contains the main method.
+    //</summary>
     class Program
     {
+        //<summary>
+        //This is the main method.
+        //</summary>
+        //<param name="args">The arguments passed to the application (not used).</param>
         static void Main(string[] args)
         {
             //LoginMenu.ShowLoginMenu();
