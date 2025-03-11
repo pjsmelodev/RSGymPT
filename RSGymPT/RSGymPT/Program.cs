b198b3c2720a5dc2caaed2b41ea886809208c5fa
pjsm <pjsm.lesi@gmail.com>
2025-03-11 17:17:00 +0000

    UPDATED: Program CREATED: Utils/ and Helpers

--- RSGymPT/RSGymPT/Program.cs
+++ RSGymPT/RSGymPT/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using RSGymPT.Utils;
+
+namespace RSGymPT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Helpers.PauseConsole();
+        }
+    }
+}
---
+++ RSGymPT/RSGymPT/Utils/Helpers.cs
@@ -0,0 +1,30 @@
+﻿namespace RSGymPT.Utils
+{
+    //<summary>
+    //This class contains helper methods that are used throughout the application.
+    //</summary>
+    public class Helpers
+    {
+        //<summary>
+        //This method pauses the console until a key is pressed.
+        //</summary>
+        public static void PauseConsole()
+        {
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey(true);
+        }
+
+        //<summary>
+        //This method prints a formated title to the console.
+        //</summary>
+        //<param name="title">The title to be printed.</param>
+        public static void PrintTitle(string title)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(new string('=', title.Length + 4));
+            Console.WriteLine($"| {title.ToUpper()} |");
+            Console.WriteLine(new string('=', title.Length + 4));
+            Console.ResetColor();
+        }
+    }
+}
