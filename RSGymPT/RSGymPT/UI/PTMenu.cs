b73f5174ede9b8ee760c6224e6f0f7229735b0ed
pjsm <pjsm.lesi@gmail.com>
2025-03-11 23:27:39 +0000

    CREATED: PTMenu, UserMenu

---
+++ RSGymPT/RSGymPT/UI/PTMenu.cs
@@ -0,0 +1,48 @@
+﻿using RSGymPT.Utils;
+
+namespace RSGymPT.UI
+{
+    //<summary>
+    //This class contains the methods to display the personal trainer menu.
+    //</summary>
+    public class PTMenu
+    {
+        public static void ShowPTMenu()
+        {
+            while (true)
+            {
+                Console.Clear();
+
+                Helpers.PrintTitle("RSGymPT - Personal Trainer Menu");
+                Console.WriteLine();
+                Console.WriteLine("1. Search a personal trainer by code");
+                Console.WriteLine("2. List all personal trainers");
+                Console.WriteLine("0. Exit");
+                Console.Write("Chose an option: ");
+
+                switch (Console.ReadLine())
+                {
+                    case "1":
+                        Console.Clear();
+                        // TODO: implementar logica
+                        Helpers.PauseConsole();
+                        break;
+                    case "2":
+                        Console.Clear();
+                        // TODO: implementar logica
+                        Helpers.PauseConsole();
+                        break;
+                    case "0":
+                        Console.WriteLine("Exiting...");
+                        Helpers.PauseConsole();
+                        MainMenu.ShowMainMenu();
+                        return;
+                    default:
+                        Console.WriteLine("Invalid option. Try again.");
+                        Helpers.PauseConsole();
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ RSGymPT/RSGymPT/UI/UserMenu.cs
@@ -0,0 +1,44 @@
+﻿using RSGymPT.Utils;
+
+namespace RSGymPT.UI
+{
+    //<summary>
+    //This class contains the methods to display the user menu.
+    //</summary>
+    public class UserMenu
+    {
+        //<summary>
+        //This method displays the user menu.
+        //</summary>
+        public static void ShowUserMenu()
+        {
+            while (true)
+            {
+                Console.Clear();
+
+                Helpers.PrintTitle("RSGymPT - User Menu");
+                Console.WriteLine();
+                Console.WriteLine("1. List all users");
+                Console.WriteLine("0. Exit");
+                Console.Write("Chose an option: ");
+
+                switch (Console.ReadLine())
+                {
+                    case "1":
+                        Console.Clear();
+                        // TODO: implementar logica
+                        break;
+                    case "0":
+                        Console.WriteLine("Exiting...");
+                        Helpers.PauseConsole();
+                        LoginMenu.ShowLoginMenu();
+                        return;
+                    default:
+                        Console.WriteLine("Invalid option. Try again.");
+                        Helpers.PauseConsole();
+                        break;
+                }
+            }
+        }
+    }
+}
