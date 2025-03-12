79810a5a9cf839c8f12cf41a817c57e34c753644
pjsm <pjsm.lesi@gmail.com>
2025-03-12 19:09:47 +0000

    CREATED: several models UPDATED: several .json files

---
+++ RSGymPT/RSGymPT/Models/PersonalTrainer.cs
@@ -0,0 +1,13 @@
+﻿namespace RSGymPT.Models
+{
+    //<summary>
+    //This class represents a personal trainer.
+    //</summary>
+    public class PersonalTrainer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Phone { get; set; }
+        public string PTCode { get; set; }
+    }
+}
---
+++ RSGymPT/RSGymPT/Models/User.cs
@@ -0,0 +1,14 @@
+﻿namespace RSGymPT.Models
+{
+    //<summary>
+    //This class represents a user of the application.
+    //</summary>
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string UserCode { get; set; }
+        public string Password { get; set; }
+    }
+}
